d374205fdf5fc569c0967a230f629dd57ed5b2fb
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-11-24 23:56:25 -0300

    Create 1-texto.py

---
+++ Analise-de-Dados-IV-Streamlit/1-texto.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write('Hello World')
+st.write('Testando o Streamlit')
+
+st.title('App Streamlit')
+st.markdown('Streamlit é **divertido**')
+st.header('Adicionando um cabeçalho')
+st.subheader('Adicionando um subcabeçalho')
+st.caption('O texto está pequeno')
+st.code('v=Fulano')
+st.text('Olá mundo')
