6d9f7fe425284d54d7c642d4067242c88c7941eb
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-12-05 02:00:44 -0300

    Update 5-layout.py

--- Analise-de-Dados-IV-Streamlit/5-layout.py
+++ Analise-de-Dados-IV-Streamlit/5-layout.py
@@ -15,3 +15,24 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+    
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("Desenvolvimento Assistente Virtual com Python")
+    
+with col2:
+    st.header("Desenvolvimento Web com Flask")
+    
+st.header("Utilizando Expander")
+
+st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write('''
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    ''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+    
