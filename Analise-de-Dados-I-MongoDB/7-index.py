f94fa35304f90c6af00c52484166207b33a154f2
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-12-24 18:12:07 -0300

    Update 7-index.py

--- Analise-de-Dados-I-MongoDB/7-index.py
+++ Analise-de-Dados-I-MongoDB/7-index.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import timeit
 
 # 1 - Estabelece conexão com o mongoDB e o Database
+
 client = MongoClient()
 db = client['nobel']
 
@@ -9,22 +10,26 @@ prizes = db['prizes']
 laureates = db['laureates']
 
 # 1 - Busca prêmios de 1910
+
 def get_prizes():
     list(db.laureates.find({'year':'1910'}))
     
 # 2 - Função para medir o tempo de execução
+
 def measure_execution_time(function):
     execution_time = timeit.timeit(function , globals=globals(),
                                    number=1)*1000
     print(f'Tempo de execução:{execution_time:.2f} milissegundo')
     
 # 3 - Sem e com índice
+
 measure_execution_time('get_prizes()')
 
 db.prizes.create_index([('year', 1)])
 measure_execution_time('get_prizes()')
 
 # 4 - Criando um índice composto
+
 db.prizes.create_index([('category', 1), ('year', 1)])
 
 def get_all_prizes_economics():
