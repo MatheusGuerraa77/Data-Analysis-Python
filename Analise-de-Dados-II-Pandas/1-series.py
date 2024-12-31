0a7f0e6fd504b13ca113038f69002955ce2d4a0f
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-12-31 01:31:26 -0300

    Update 1-series.py

--- Analise-de-Dados-II-Pandas/1-series.py
+++ Analise-de-Dados-II-Pandas/1-series.py
@@ -8,6 +8,7 @@ Funcionalidade de Series:
 import pandas as pd
 
 # 1 - Dados dos times e suas quantidades de títulos
+
 dados = {
     'Real Madrid': 34,
     'Barcelona': 26,
@@ -17,18 +18,22 @@ dados = {
 }
 
 # 2 - Criando um serie a partir de um dicionário
+
 series_times = pd.Series(dados)
 print(series_times)
 print(type(series_times))
 
 # 3 - Selecionando time por índice
+
 print(series_times['Juventus'])
 print(series_times.iloc[2])
 
 # 4 - Selecionando por fatiamento
+
 print('\n')
 print(series_times['Barcelona':'Juventus']) # inclusivo
 
 # 5 - Selecionar por condição
+
 print('\n')
 print(series_times[series_times > 30])
