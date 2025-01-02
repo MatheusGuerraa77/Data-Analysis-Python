112d8014bfcde7487658161f65b68eb5f2867e60
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-01 23:58:19 -0300

    Create 2-dataframe.py

---
+++ Analise-de-Dados-II-Pandas/2-dataframe.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# 1 - Dados dos times e suas quantidades de títulos
+
+dados_titulos = {
+    'Real Madrid': 34,
+    'Barcelona': 26,
+    'Liverpool': 19,
+    'Juventus': 36,
+    'Bayern Munich': 30
+}
+
+# 2 - Ano do título
+dados_anos = {
+    'Real Madrid': [1956, 1957, 1958, 1959, 1960],
+    'Barcelona': [1992, 2006, 2009, 2011, 2015],
+    'Liverpool': [1977, 1978, 1981, 1984, 2005],
+    'Juventus': [1958, 1985, 1996, 2011, 2015],
+    'Bayern Munich': [1974, 1975, 1976, 2001, 2003]
+}
