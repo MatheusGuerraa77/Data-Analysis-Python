42d2cc2301864d2f5fcbe2dba77560d8153afa61
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-05 23:57:03 -0300

    Update 4-operacoes_df.py

--- Analise-de-Dados-II-Pandas/4-operacoes_df.py
+++ Analise-de-Dados-II-Pandas/4-operacoes_df.py
@@ -6,7 +6,7 @@ data = {
     'Nome': ['Alice', 'Bob', 'Charlie', 'David', 'Emily'],
     'Idade': [25, 30, 35, 40, 27],
     'Cargo': ['Analista', 'Gerente', 'CEO', 'Analista', 'Coordenador'],
-    'Salario': [5000, 8000, 15000, 4800, 6000]
+    'Salario': [5000, 8000, 15000, 4800, 7000]
 }
 
 df = pd.DataFrame(data)
