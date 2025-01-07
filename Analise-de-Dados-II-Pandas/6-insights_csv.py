1cbfd0c2546f190ae89f6e3103ec9fd42e4986d7
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-07 13:26:45 -0300

    Create 6-insights_csv.py

---
+++ Analise-de-Dados-II-Pandas/6-insights_csv.py
@@ -0,0 +1,20 @@
+#Insights:
+#1-Desempenho Vendas por Região
+#2-Vendedor mais produtivo
+#3-Itens mais vendidos
+#4-Preço Médio por item
+#5-Correlação entre unidade Vendida e Preço Unitário
+
+import pandas as pd
+df = pd.read_csv('data1/pedidos.csv')
+
+df['Unidades'] = pd.to_numeric(df['Unidades'])
+df['PrecoUnidade'] = pd.to_numeric(df['PrecoUnidade'])
+
+#1 - Desempenho Vendas por Região
+venda_regiao = df.groupby('Regiao')['Unidades'].sum()
+print(venda_regiao)
+
+# 2 - Vendedor mais produtivo
+vendas_vendedor = df.groupby('Vendedor')['Unidades'].sum()
+print(vendas_vendedor.idxmax())
