a141bb948f7f9b96b1183c90441643e8afef726c
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-07 13:18:28 -0300

    Update 5-importa_csv.py

--- Analise-de-Dados-II-Pandas/5-importa_csv.py
+++ Analise-de-Dados-II-Pandas/5-importa_csv.py
@@ -8,3 +8,19 @@ print(type(df))
 # 2 - Importando dataset com delimitador ponto e vírgula
 df2 = pd.read_csv('data1/pedidos2.csv', sep=';')
 print(df2)
+
+# 3 - Lê primeiros registros
+print(df.head(10))
+
+# 4 - Lê últimos registros
+print(df.tail())
+
+print(df.shape)
+
+print(df.dtypes)
+
+# 5 - Ordenação de Valores
+print(df.sort_values(by='Unidades', ascending=False))
+
+# 6 - Estados com mais vendas
+print(df['Estado'].value_counts())
