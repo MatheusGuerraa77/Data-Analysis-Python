bf0fd1855e583d1034161cbab386e2ed435d0200
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-10 16:29:33 -0300

    Create 8-importa_planilha.py

---
+++ Analise-de-Dados-II-Pandas/8-importa_planilha.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Caminho para o arquivo Excel
+caminho_arquivo = 'data1/livros.xlsx'
+df = pd.read_excel(caminho_arquivo)
+
+print(df)
+
+
+# 1 - Visão geral dos dados
+print(df.head())
+print(df.tail())
+
+# 2 - Verificando tipos de dados
+print(df.dtypes)
+
+# 3 - Estatísticas Descritivas
+print(df.describe())
+print(df[['Preço (R$)', 'Quantidade']].describe())
