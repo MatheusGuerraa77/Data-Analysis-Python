e62399e6a851acaacbf9c5bd273007eeea2703cb
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-01-16 02:36:39 -0300

    commit bd

--- Analise-de-Dados-II-Pandas/13-bd.py
+++ Analise-de-Dados-II-Pandas/13-bd.py
@@ -4,4 +4,7 @@ from sqlalchemy import create_engine
 
 engine = create_engine("sqlite:///pedidos.db")
 
-df = pd.read_csv('data1/pedidos.csv')
+df = pd.read_csv('data1/Pedidos.csv')
+
+# 1 - Exportando para tabela
+df.to_sql('pedidos', engine, index=False)
