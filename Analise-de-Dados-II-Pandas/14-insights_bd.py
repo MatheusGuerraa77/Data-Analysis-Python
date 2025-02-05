d4972ad45e637ca3509661acfb06bfdaa0becfeb
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-02-04 22:24:44 -0300

    Update 14-insights_bd.py

--- Analise-de-Dados-II-Pandas/14-insights_bd.py
+++ Analise-de-Dados-II-Pandas/14-insights_bd.py
@@ -4,12 +4,14 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///pedidos.db')
 
 # 1 - Total de vendas por região
+
 consulta_1 = 'SELECT Regiao, SUM(Unidades) AS Total_Unidades_Vendidas FROM pedidos GROUP BY Regiao'
 resultado_1 = pd.read_sql_query(consulta_1, engine)
 print('Total de Vendas por região: \n')
 print(resultado_1)
 
 # 2 - Vendedor com o maior número de vendas 
+
 consulta_2 =  'SELECT Vendedor, SUM(Unidades) AS Total_Unidades_Vendidas FROM pedidos GROUP BY Vendedor ORDER BY Total_Unidades_Vendidas DESC LIMIT 1'
 resultado_2 = pd.read_sql_query(consulta_2, engine)
 print(resultado_2)
