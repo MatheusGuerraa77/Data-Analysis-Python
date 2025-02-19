eb8e024ff0aeaa61c5f80bf4e871ae0c35df3483
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-02-19 13:59:07 -0300

    Create 5-insights_dataset.py

---
+++ Analise-de-Dados-III-DataView/5-insights_dataset.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data1/pedidos.csv')
+
+# Gráfico 1 - Quantidade de unidades vendidas por região
+plt.figure(figsize=(8, 6))
+df.groupby('Regiao')['Unidades'].sum().plot(kind='bar', color='skyblue')
+plt.title('Quantidade de Unidades Vendidas por Regiao')
+plt.xlabel('Região')
+plt.ylabel('Total de Unidades Vendidas')
+plt.xticks(rotation=45)
+plt.show()
+
+# Gráfico 2 - Distribuição das vendas por item(pizza)
+plt.figure(figsize=(8, 6))
+df['Item'].values_counts().plot(kind='pie', autopct='%1.1f%%', startangle=90)
+plt.title('Distribuição das Vendas por Item')
+plt.axis('equal')
+plt.show()
+
+# Gráfico 3 - Relação entre preço unitário e quantidade de unidades
+plt.figure(figsize=(8, 6))
+plt.scatter(
+    df['PrecoUnidade'],
+    df['Unidades'],
+    color='orange'
+)
+plt.title('Relação entre Preço Unitário e Quantidade de Unidades')
+plt.xlabel('Preço Unitário')
+plt.ylabel('Quantidade de Unidades')
+plt.grid(True)
+plt.show()
