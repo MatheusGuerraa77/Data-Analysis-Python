2c88c611e0ede8d8a43711f35cde025b45fef4d0
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-02-20 15:46:53 -0300

    Update 5-insights_dataset.py

--- Analise-de-Dados-III-DataView/5-insights_dataset.py
+++ Analise-de-Dados-III-DataView/5-insights_dataset.py
@@ -31,3 +31,32 @@ plt.xlabel('Preço Unitário')
 plt.ylabel('Quantidade de Unidades')
 plt.grid(True)
 plt.show()
+
+# Gráfico 4- Quantidade de unidades vendidas ao longo do tempo
+# Convertendo a coluna DataPedido para o formato de data
+
+df['DataPedido'] = pd.to_datetime(df['DataPedido'])
+
+plt.figure(figsize=(10, 6))
+df.groupby('DataPedido')['Unidades'].sum().plot(kind='line', marker='o', color='green')
+plt.title('Quantidade de Unidades Vendidas ao Longo do Tempo')
+plt.xlabel('Data do Pedido')
+plt.ylabel('Total de Unidades Vendidas')
+plt.grid(True)
+plt.show()
+
+# Gráfico 5 - Qunatidade de unidades Vendidas por Estado em cada região
+pivot = df.pivot_table(
+    index='Estado',
+    columns='Regiao',
+    values='Unidades',
+    oggfunc='sum',
+    fill_value=0
+)
+plt.figure(figsize=(10, 6))
+pivot.plot(kind='bar', stacked=True)
+plt.title('Quantidade de Unidades Vendidas por Estado em cada Região')
+plt.xlabel('Estado')
+plt.ylabel('Total de Unidades Vendidas')
+plt.legend(title='Regiao', Loc='upper left', bbox_to_anchor=(1.05, 1))
+plt.xticks(rotation=45)
