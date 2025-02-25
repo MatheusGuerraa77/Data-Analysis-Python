9b8fb76184383ede87a1b212d41161b6abd5caa1
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-02-24 23:34:16 -0300

    Update 6-fig.py

--- Analise-de-Dados-III-DataView/6-fig.py
+++ Analise-de-Dados-III-DataView/6-fig.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1 - Dados fictícios para os gráficos
+
 x = np.arange(1, 6)
 y1 = np.array([3, 5, 9, 7, 3])
 y2 =np.array([1, 6, 2, 8, 4])
@@ -10,10 +11,12 @@ y2 =np.array([1, 6, 2, 8, 4])
 fig, ax = plt.subplots(2, figsize=(8, 8))
 
 # 3 - Gráfico de barras no subplot superior
+
 ax[0].bar(x, y1, color='skyblue')
 ax[0].set_title('Gráfico de Barras')
 
 # 4 - Gráfico de linha no subplot inferior
+
 ax[1].plot(x, y2, marker='o', linestyle='-', color='green')
 ax[1].set_title('Gráfico de Linha')
 
